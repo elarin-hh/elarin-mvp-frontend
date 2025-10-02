1ff34f8f3b53b98863828da28d2a5ee2fe0c1233
Eduardo Borges <eduardo@cubosis.com.br>
2025-10-02 12:00:38 -0300

    page deploy actions

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,11 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    }),
     prerender: {
       handleMissingId: 'warn',
       handleHttpError: 'warn',
