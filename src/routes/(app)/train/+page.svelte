f7859c1eaa5708a4e9eb5b2c503d38348a36f90f
Eduardo Borges <eduardo1409borges@gmail.com>
2025-12-07 01:48:00 -0300

    ui/ux designer

--- src/routes/(app)/train/+page.svelte
+++ src/routes/(app)/train/+page.svelte
@@ -1471,7 +1471,7 @@
 
   .card {
     backdrop-filter: blur(15px) saturate(75%);
-    /* -webkit-backdrop-filter: blur(15px) saturate(45%); */
+    -webkit-backdrop-filter: blur(15px) saturate(75%);
     background-color: rgba(0, 0, 0, 0.4);
     border-radius: 20px;
     border: 1px solid rgba(255, 255, 255, 0.18);
